fb88268c6ec48bedf710175518a1c20869c254ee
iSemicolon <palash.samanta7497@gmail.com>
2023-04-16 14:53:00 +0530

    "SPRING-BOOT-SECURITY: Authorization"

---
+++ Spring-Boot-Security-Authorization/src/main/java/org/kol/security/controller/HomeController.java
@@ -0,0 +1,28 @@
+package org.kol.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return ("Welcome");
+    }
+
+
+    @GetMapping("/user")
+    public String user() {
+        return ("Welcome User");
+    }
+
+
+    @GetMapping("/admin")
+    public String admin() {
+        return ("Welcome Admin");
+    }
+
+
+
+}
